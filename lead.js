f3a62f522747435d704a3d778b73212c49600f3b
Prashanth <prashanthgatadi@gmail.com>
2025-02-05 09:08:25 +0530

    reCaptcha Validation

--- lead.js
+++ lead.js
@@ -1,11 +1,17 @@
-function beforesubmit() {
-  let inputdate = document.querySelector(".inputdate");
-  let outputdate = document.querySelector(".outputdate");
-  console.log(inputdate.value);
-  console.log(typeof inputdate.value); //convert string to local date
-  let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formatedDate;
-  console.log(outputdate.value);
+let capthachecked = false;
+function beforesubmit(event) {
+  if (capthachecked) {
+    let inputdate = document.querySelector(".inputdate");
+    let outputdate = document.querySelector(".outputdate");
+    console.log(inputdate.value);
+    console.log(typeof inputdate.value); //convert string to local date
+    let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatedDate;
+    console.log(outputdate.value);
+  } else {
+    alert("Please check the reCAPTHACA box to submit the lead");
+    event.preventDefault();
+  }
 }
 function timestamp() {
   var response = document.getElementById("g-recaptcha-response");
@@ -19,3 +25,6 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+function captachaSuccess() {
+  capthachecked = true;
+}
